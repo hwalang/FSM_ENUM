c78969fe655ae11ff7dc2b1bdbb77f8cc8342982
hwalang <smdong97@gmail.com>
2024-04-09 14:18:23 +0900

    Feat: State

--- Assets/Scripts/FSM/State.cs
+++ Assets/Scripts/FSM/State.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class State : MonoBehaviour
+public abstract class State : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isCompelete { get; protected set; }
+
+    // Check when the state started and how long it has been running.
+    protected float startTime;
+    public float time => Time.time - startTime;
+
+    // Variables common to all states
+    protected Rigidbody2D body;
+    protected Animator animator;
+    protected PlayerController controller;   // Can pass on all the information possessed by the PlayerController instance.
+
+    public virtual void Enter() { }
+    public virtual void Do() { }
+    public virtual void FixedDo() { }
+    public virtual void Exit() { }
+
+    public void Init(Rigidbody2D _body, Animator _animator, PlayerController _controller)
     {
-        
+        body = _body;
+        animator = _animator;
+        controller = _controller;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Set when the current state started.
+    public void Initialize()
     {
-        
+        isCompelete = false;
+        startTime = Time.time;
     }
 }
